ac334f1ec699f5e096ff50504e1577492dc54334
afolabi-ola <148984714+afolabi-ola@users.noreply.github.com>
2024-05-28 17:01:06 +0100

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -15,9 +15,6 @@ function getTodos() {
   if (dataBaseTodos) {
     localDb = [...JSON.parse(dataBaseTodos)];
     ul.innerHTML = localDb.map(renderTodos).join("");
-  } else {
-    addTodosToLocalStorage(localDb);
-    console.log(dataBaseTodos);
   }
    checkSort();
 }
